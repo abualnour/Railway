bf1c3e49028f51945a00c5f830f790702d550e97
abualnour <33073799+abualnour@users.noreply.github.com>
2026-04-11 04:51:17 +0300

    Fix Render media disk path

--- config/settings.py
+++ config/settings.py
@@ -121,7 +121,7 @@ STORAGES = {
 }
 
 MEDIA_URL = "media/"
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = Path(os.environ.get("RENDER_DISK_MOUNT_PATH", "/var/data")) / "media"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
