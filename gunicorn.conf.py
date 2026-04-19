2595458d624d0296dfd287bf8782bb24f889d24e
abualnour <33073799+abualnour@users.noreply.github.com>
2026-04-20 02:01:17 +0300

    app memoery issue

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,13 +1,12 @@
-import multiprocessing
 import os
 
-
 bind = f"0.0.0.0:{os.environ.get('PORT', '8000')}"
-workers = int(os.environ.get("GUNICORN_WORKERS", max(2, multiprocessing.cpu_count() // 2)))
+workers = int(os.environ.get("GUNICORN_WORKERS", "1"))
 threads = int(os.environ.get("GUNICORN_THREADS", "2"))
 timeout = int(os.environ.get("GUNICORN_TIMEOUT", "120"))
-graceful_timeout = int(os.environ.get("GUNICORN_GRACEFUL_TIMEOUT", "30"))
+graceful_timeout = int(os.environ.get("GUNICORN_GRACEFUL_TIMEOUT", "120"))
 keepalive = int(os.environ.get("GUNICORN_KEEPALIVE", "5"))
 loglevel = os.environ.get("GUNICORN_LOG_LEVEL", "info")
 accesslog = "-"
 errorlog = "-"
+worker_class = "gthread"
